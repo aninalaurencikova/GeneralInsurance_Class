911fe75f663dcc9b4b6509eefcc63c71c188339e
aninalaurencikova <laurencikova@gmail.com>
2019-03-05 07:17:26 +0100

    check for HW

--- Lessons/Lesson2/Homework/Homework_1.R
+++ Lessons/Lesson2/Homework/Homework_1.R
@@ -3,32 +3,39 @@
 # __Commit__ it to your repository into `Lessons/Lesson2/Homework`.
 
 ## Code
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
+library(dplyr)
+library(ggplot2)
 
+#dt_KPI_raw <- read.csv("./Data/lesson2_KPI.csv")
+dt_KPI <- read.csv("C:/Users/laure/Desktop/Anina_github/GeneralInsurance_Class/Data/lesson2_KPI.csv")
 
+dt_KPI_raw %>% 
+  mutate(Premium = ifelse(Premium < 0, 0, Premium))
 
+# najprofitabilnejsie portfolio 7
+dt_KPI_raw %>%  mutate(UWR = Premium - Expenses - Losses) %>% 
+  group_by(Unit) %>% 
+  summarize(UWR = sum(UWR, na.rm = TRUE)) %>% 
+  arrange(UWR)
+
+dt_KPI_raw %>%  mutate(UWR = Premium - Expenses - Losses) %>% 
+  filter(Unit == "Unit7") %>%
+  group_by(Year) %>% 
+  summarize(UWR = sum(UWR, na.rm = TRUE)) %>% 
+  arrange(UWR)
+
+dt_KPI_raw %>% 
+  mutate(UWR = Premium - Expenses - Losses) %>% 
+  filter(Unit == "Unit7") %>%
+  group_by(Year) %>% 
+  summarize(UWR = sum(UWR, na.rm = TRUE)) %>% 
+  ggplot(aes(x = reorder(Year, UWR), y = UWR)) + 
+  geom_col()
 
 # Your Explanation about analysis:
-# 
-# 
-# 
+# Najprv sme dáta očistili od záporného poistného. 
+# Potom sme zistili, ktoré portfólo bolo najprofitabilnejšie (Unit 7).
+# Vyfiltrovali sme si UWR pre Unit 7, groupli sme ich podľa rokov. 
+# Najlepším rokov podľa UWR bol 2013, najhorším 2014. 
+
+# Podľa analýzy: Najhorším rokom bol 2015, pretože má najmenšiu hodnotu UWR.
